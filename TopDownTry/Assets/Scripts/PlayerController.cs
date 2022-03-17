60e4a766900d7ae02e7d255711ead322537690c5
ClaudiuZabava <zabavaclaudiu4@gmail.com>
2022-03-18 00:13:21 +0200

    Update

---
+++ TopDownTry/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float  speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+        RotationInput();
+    }
+    void MovementInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(x, 0f,z).normalized;
+
+        transform.Translate(dir *speed* Time.deltaTime, Space.World );
+
+
+    }
+
+    void RotationInput()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+}
