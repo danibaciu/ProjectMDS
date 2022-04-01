bb9394570c95a62cfbc9ff3a8fe7e171e2846659
aleksandeerG <82213474+aleksandeerG@users.noreply.github.com>
2022-04-01 10:04:24 +0300

    Enemies following in range

--- TopDownTry/Assets/Scripts/Enemy.cs
+++ TopDownTry/Assets/Scripts/Enemy.cs
@@ -11,36 +11,45 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     public float waitTime;
 
+    [SerializeField]
+    private GameObject corp;
+
     private float currentTime;
     private GameObject player;
     private bool shot;
-
-    
+    private Animator anim;
+    private float curent_x=0;
+    private float curent_z=0;
     public float minDistance;
-
     //public Transform target;
 
     NavMeshAgent nav;
-
+   
     //Methods
 
-    
     public void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = corp.GetComponent<Animator>();
         player = GameObject.FindWithTag("Player");
         
-
-
     }
-    
     public void Update()
     {
+        float x = transform.position.x;
+        float z = transform.position.z;
+
+        anim.SetFloat("Speed", Mathf.Abs(x-curent_x)+Mathf.Abs(z-curent_z)); 
+
+        curent_x = x;
+        curent_z = z;
+
+
         if(health <= 0)
         {
             Die();
         }
-        
+        this.transform.LookAt(player.transform);
         if(currentTime==0)
         {
             Shoot();
@@ -49,10 +58,9 @@ public class Enemy : MonoBehaviour
             currentTime += 1 * Time.deltaTime;
         if (currentTime >= waitTime)
             currentTime = 0;
-        
 
         float distance = Vector3.Distance(player.transform.position, transform.position);
-        
+
         if (distance < minDistance && player.transform.hasChanged)
         {
             nav.isStopped = false;
@@ -60,14 +68,10 @@ public class Enemy : MonoBehaviour
             this.transform.LookAt(player.transform);
             nav.SetDestination(player.transform.position);
 
-            
+
         }
         else
             nav.isStopped = true;
-       
-        
-            
-        
     }
 
     //trebuie scriptul de shoot si bullet de la Dani
